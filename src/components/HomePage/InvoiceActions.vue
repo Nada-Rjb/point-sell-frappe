b335dba3e8d7e2d56d4f2e13f4419c1f168c78e5
Nada-Rjb <enadaragab2020@gmail.com>
2025-03-16 17:21:02 +0200

    feat: add InvoiceActions component to HomeView and implement invoice action buttons

---
+++ src/components/HomePage/InvoiceActions.vue
@@ -0,0 +1,33 @@
+<template>
+  <section class="flow section" data-padding="compact">
+    <div class="wrapper" id="main">
+      <div class="invoice-actions">
+        <button class="show-invoices">📄 عرض الفواتير</button>
+        <button class="upload-invoice">⬆️ رفع الفاتورة</button>
+      </div>
+    </div>
+  </section>
+</template>
+<style scoped>
+.invoice-actions {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  gap: 2rem;
+}
+button {
+  width: 153px;
+  height: 40px;
+  border-radius: var(--border-radius-1);
+}
+.show-invoices {
+  background-color: black;
+  color: white;
+  border: 1px solid #2c2c2c;
+  border-radius: var(--border-radius-2);
+}
+.upload-invoice {
+  border: 1px solid #d9d9d9;
+}
+</style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -3,18 +3,21 @@
   <app-search></app-search>
   <customer-info></customer-info>
   <DebtSummary></DebtSummary>
+  <InvoiceActions></InvoiceActions>
 </template>
 
 <script>
 import AppSearch from "@/components/HomePage/AppSearch.vue";
 import CustomerInfo from "@/components/HomePage/CustomerInfo.vue";
 import DebtSummary from "@/components/HomePage/DebtSummary.vue";
+import InvoiceActions from "@/components/HomePage/InvoiceActions.vue";
 export default {
   name: "HomeView",
   components: {
     AppSearch,
     CustomerInfo,
     DebtSummary,
+    InvoiceActions,
   },
 };
 </script>
